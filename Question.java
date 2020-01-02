853fc57cbd1adea0d63f38b9512f12aa607c1aa9
sandunsameera <sandunsameera25@gmail.com>
2020-01-02 22:06:27 +0530

    Tried to implement hashed function

---
+++ Question.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class Question {
+    public static void main(String[] args) {
+
+
+        int array[] = new int[] {
+            962712,
+            308682,
+            203292,
+            368620,
+            315274,
+            425493,
+            258944,
+            681200,
+            626376,
+            876039,
+            948267,
+            994726,
+            900631,
+            632986,
+            504507,
+            819596,
+            29333,
+            729171,
+            422843,
+            692730,
+            675279,
+            99683,
+            426655,
+            814586,
+            561288,
+            231395,
+            102916,
+            443695,
+            277089,
+            492037,
+            893523};
+
+            int sums[] = new int[]{31552,234756,596873,648219,726312,981237,988331,1277361,1283379};
+            _hashFunction(array);
+    }
+
+    
+    static void _hashFunction(int arr[]){
+        int n = 31;
+        
+        for(int i=0;i<arr.length;i++){
+            int hashedvalue = (arr[i]%23)%n;
+            System.out.println(hashedvalue);
+            for(int j=0;j<n;j++){
+                if(j==n-1){
+                    System.out.println("Error cuz collisions " + arr[i] );
+                    break;
+                }else{
+                int hashedtable[][]= new int [j][hashedvalue]; 
+                }
+            }
+        }
+        
+    }
+}
