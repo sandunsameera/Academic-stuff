e5239004b12d6f20b6966ffbe8c2050125f2b6de
sandunsameera <sandunsameera25@gmail.com>
2020-01-11 21:47:26 +0530

    Implemented fully for dummy data

--- test.c
+++ test.c
@@ -3,6 +3,8 @@
 
 typedef struct Node node;
 
+int flag ;
+
 struct Node
 {
     node* next;
@@ -18,7 +20,7 @@ int main(){
     for(int i=0;i<7;i++){
         array[i].value = NULL;
     }
-    int numarray[] = {962712, 308682, 203292, 368620, 315274, 425493, 258944, 681200, 626376, 876039,
+    int numarray[] = {962712, 2219,307131,308682, 203292, 368620, 315274, 425493, 258944, 681200, 626376, 876039,
                 948267, 994726, 900631, 632986, 504507, 819596, 29333, 729171, 422843, 692730, 675279, 99683, 426655,
                 814586, 561288, 231395, 102916, 443695, 277089, 492037, 893523 };
 
@@ -43,7 +45,7 @@ int main(){
 
     }
 
-    for(int i = 0; i <7 ; i++){
+    for(int i = 0; i < 7; i++){
         printf("%d ",i);
         node* ptr = &array[i];
         while (ptr->next != NULL){
@@ -51,31 +53,42 @@ int main(){
             ptr = ptr->next;
         }
         printf("\n");
-    }
+    }  
 
-    for (int i = 0; i < sizeof(sums); i++)
+    for (int i = 0; i < 9; i++)
     {
-        for (int k = 0; k < sizeof(numarray); k++)
+        for (int k = 0; k < 31; k++)
         {
-            int secondnumber = (sums[i]- numarray[k]);
+            int secondnumber = (sums[i] - numarray[k]);
             if (secondnumber>0)
             {
-            // printf("%d - %d\n",i,secondnumber);
-            int hashedsecondNumber=hash(secondnumber);
-            // printf("%d - %d\n",hashedsecondNumber,secondnumber);
-                node* ptr = &array[hashedsecondNumber];
-                while (ptr->next != NULL){
-                    if(ptr->value == secondnumber){
-
-                printf("found");
-                    }
+            int hashedSecondNumber = hash(secondnumber);
+            // printf("%d - %d\n",hashedSecondNumber,secondnumber);
+            node* ptr =&array[i];
+            if (ptr->value == secondnumber)
+            {
+                flag =1;
+            }else
+            {
+                // printf("Not Found\n");
                 ptr = ptr->next;
-        }
-            
-            break;
+                flag =0;
             }
         }
-        
+            
+    }
+
+    if (flag ==0)
+    {
+    printf("%d",0);
+    }else
+    {
+    printf("%d",1);   
     }
     
+    
+        
+}
+    
+  
 }
