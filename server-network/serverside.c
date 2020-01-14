1cb1e1b5581b0ef76fd624b2eaecc411c0559adc
sandunsameera <sandunsameera25@gmail.com>
2020-01-14 22:24:51 +0530

    Server modified

--- server-network/serverside.c
+++ server-network/serverside.c
@@ -60,5 +60,5 @@ int main(int argc, char const *argv[])
         printf("------------------Hello message sent-------------------");
         close(new_socket);
     }
-    return 0;
+    return 0;   
 }
